8b266d6975f164b0e7a3d927f2a18d03472df4a9
GushALKDev <gustavomartindeveloper@gmail.com>
2025-10-14 16:43:43 +0200

    Section 2: Swap - Completed

--- foundry/test/uniswap-v2/exercises/UniswapV2Swap.test.sol
+++ foundry/test/uniswap-v2/exercises/UniswapV2Swap.test.sol
@@ -52,6 +52,19 @@ contract UniswapV2SwapTest is Test {
         uint256 amountOutMin = 1;
 
         // Write your code here
+        vm.prank(user);
+        uint256[] memory amounts = router.swapExactTokensForTokens({
+            amountIn: amountIn,
+            amountOutMin: amountOutMin,
+            path: path,
+            to: user,
+            deadline: block.timestamp
+        });
+        vm.stopPrank();
+
+        console2.log("WETH", amounts[0]);
+        console2.log("DAI", amounts[1]);
+        console2.log("MKR", amounts[2]);
         // Don’t change any other code
 
         assertGe(mkr.balanceOf(user), amountOutMin, "MKR balance of user");
@@ -69,8 +82,22 @@ contract UniswapV2SwapTest is Test {
         uint256 amountInMax = 1e18;
 
         // Write your code here
+        vm.prank(user);
+        uint256[] memory amounts = router.swapTokensForExactTokens({
+            amountOut: amountOut,
+            amountInMax: amountInMax,
+            path: path,
+            to: user,
+            deadline: block.timestamp
+        });
+        vm.stopPrank();
+
+        console2.log("WETH", amounts[0]);
+        console2.log("DAI", amounts[1]);
+        console2.log("MKR", amounts[2]);
         // Don’t change any other code
 
         assertEq(mkr.balanceOf(user), amountOut, "MKR balance of user");
+        assertLe(amounts[0], amountInMax, "WETH spent by user");
     }
 }
